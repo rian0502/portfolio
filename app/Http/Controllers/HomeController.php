431a7a71b17cec9f4d01f3cb3e423fc2fdf71040
rian0502 <muhammadfebrianhasibuan@gmail.com>
2025-03-23 08:05:13 +0700

    finish portfolio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Projects;
-use Illuminate\Http\Request;
 use App\Models\Certifications;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $data = [
             'certifications' => Certifications::all(),
             'projects' => Projects::all(),
